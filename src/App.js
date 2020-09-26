49d811f04bc12475513b1c7aea8e0b9f1db221af
Andrew Lucero <al4991@nyu.edu>
2020-09-26 14:23:43 -0500

    Basic integration of react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
-
+import About from "./components/pages/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
+
 class App extends Component {
   state = {
     users: [],
@@ -40,19 +42,35 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>App that uses Github API to search for users</p>
+    </Fragment>
+  );
+};
+export default About;
